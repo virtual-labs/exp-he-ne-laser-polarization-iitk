cd9c061bbff5458826510c6570bf48362573e53c
s0h8 <sheetal.singh458@gmail.com>
2021-05-20 14:08:17 +0530

    update data

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -1,166 +1,158 @@
-/////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////
 
-/////////////////////// Do not modify the below code ////////////////////////
+ /////////////////////// Do not modify the below code ////////////////////////
 
-/////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////
 
-(function() {
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
+ (function() {
+     function buildQuiz() {
+         // we'll need a place to store the HTML output
+         const output = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
+         // for each question...
+         myQuestions.forEach((currentQuestion, questionNumber) => {
+             // we'll want to store the list of answer choices
+             const answers = [];
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
+             // and for each available answer...
+             for (letter in currentQuestion.answers) {
+                 // ...add an HTML radio button
+                 answers.push(
+                     `<label>
+           <input type="radio" name="question${questionNumber}" value="${letter}">
+           ${letter} :
+           ${currentQuestion.answers[letter]}
+         </label>`
+                 );
+             }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
-    });
+             // add this question and its answers to the output
+             output.push(
+                 `<div class="question"> ${currentQuestion.question} </div>
+       <div class="answers"> ${answers.join("")} </div>`
+             );
+         });
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+         // finally combine our output list into one string of HTML and put it on the page
+         quizContainer.innerHTML = output.join("");
+     }
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
+     function showResults() {
+         // gather answer containers from our quiz
+         const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
-    let numCorrect = 0;
+         // keep track of user's answers
+         let numCorrect = 0;
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+         // for each question...
+         myQuestions.forEach((currentQuestion, questionNumber) => {
+             // find selected answer
+             const answerContainer = answerContainers[questionNumber];
+             const selector = `input[name=question${questionNumber}]:checked`;
+             const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
+             // if answer is correct
+             if (userAnswer === currentQuestion.correctAnswer) {
+                 // add to the number of correct answers
+                 numCorrect++;
 
-        // color the answers green
-        //answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
+                 // color the answers green
+                 //answerContainers[questionNumber].style.color = "lightgreen";
+             } else {
+                 // if answer is wrong or blank
+                 // color the answers red
+                 answerContainers[questionNumber].style.color = "red";
+             }
+         });
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
+         // show number of correct answers out of total
+         resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+     }
 
-  const quizContainer = document.getElementById("quiz");
-  const resultsContainer = document.getElementById("results");
-  const submitButton = document.getElementById("submit");
- 
-
-/////////////////////////////////////////////////////////////////////////////
-
-/////////////////////// Do not modify the above code ////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
+     const quizContainer = document.getElementById("quiz");
+     const resultsContainer = document.getElementById("results");
+     const submitButton = document.getElementById("submit");
 
 
+     /////////////////////////////////////////////////////////////////////////////
+
+     /////////////////////// Do not modify the above code ////////////////////////
+
+     /////////////////////////////////////////////////////////////////////////////
 
 
 
 
-/////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-  const myQuestions = [
+     /////////////// Write the MCQ below in the exactly same described format ///////////////
+
+
+     const myQuestions = [{
+            question: "1.Property of Polarization can be observed in:", ///// Write the question inside double quotes
+            answers: {
+                a: "Transverse wave", ///// Write the option 1 inside double quotes
+                b: "longitudinal wave", ///// Write the option 2 inside double quotes
+                c: "both", ///// Write the option 2 inside double quotes
+                d: "NOT", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
     {
-      question: "Property of Polarization can be observed in:",  ///// Write the question inside double quotes
+      question: "2.According to law of Malus the intensity of the light coming out of Analyzer is directly proportional to:",  ///// Write the question inside double quotes
       answers: {
-        a: "Transverse wave",                  ///// Write the option 1 inside double quotes
-        b: "Longitudinal wave",                  ///// Write the option 2 inside double quotes
-        c: "Both",                  ///// Write the option 3 inside double quotes
-        d: "None of these"                   ///// Write the option 4 inside double quotes
-      },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
-    },
-
-    {
-     question: "According to law of Malus the intensity of the light coming out of Analyzer is directly proportional to:",  ///// Write the question inside double quotes
-      answers: {
-        a: "Cosθ",                  ///// Write the option 1 inside double quotes
-        b: "Cos<sup>2</sup> θ",                  ///// Write the option 2 inside double quotes
-        c: "Cos<sup>3</sup> θ",                  ///// Write the option 3 inside double quotes
-        d: "None of these"                   ///// Write the option 4 inside double quotes
+        a: "Cos<sup>&theta;</sup>",                  ///// Write the option 1 inside double quotes
+        b: "Cos<sup>2</sup>theta;",                  ///// Write the option 2 inside double quotes
+        c: "Cos<sup>3</sup>theta;",
+        d: NOT
       },
       correctAnswer: "b"                ///// Write the correct option inside double quotes
-    },                                  ///// To add more questions, copy the section below 
-    									                  ///// this line
-
-
-    /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
-        till closing curly braces comma ( }, )
-
-        and paste it below the curly braces comma ( below correct answer }, ) of above 
-        question
-
-    Copy below section
-
-    {
-      question: "This is question n?",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "c"
     },
-
-    Copy above section
-
-    */
-
-
-
-
-  ];
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-/////////////////////// Do not modify the below code ////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
-
-
-  // display quiz right away
-  buildQuiz();
-
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+       {
+            question: "3. Unpolarised light is:", ///// Write the question inside double quotes
+            answers: {
+                a: "Transverse Electromagnetic wave", ///// Write the option 1 inside double quotes
+                b: "longitudinal Electromagnetic wave", ///// Write the option 2 inside double quotes
+                c: "Mechanical wave", ///// Write the option 2 inside double quotes
+                d: "NOT", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+            {
+            question: "4. Polarization cannot be observed in:", ///// Write the question inside double quotes
+            answers: {
+                a: "Light wave", ///// Write the option 1 inside double quotes
+                b: "Sound wave", ///// Write the option 2 inside double quotes
+                c: "both", ///// Write the option 2 inside double quotes
+                d: "NOT", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
 
 
-/////////////////////////////////////////////////////////////////////////////
+     ];
 
-/////////////////////// Do not modify the above code ////////////////////////
 
-/////////////////////////////////////////////////////////////////////////////
+
+
+     /////////////////////////////////////////////////////////////////////////////
+
+     /////////////////////// Do not modify the below code ////////////////////////
+
+     /////////////////////////////////////////////////////////////////////////////
+
+
+     // display quiz right away
+     buildQuiz();
+
+     // on submit, show results
+     submitButton.addEventListener("click", showResults);
+ })();
+
+
+ /////////////////////////////////////////////////////////////////////////////
+
+ /////////////////////// Do not modify the above code ////////////////////////
+
+ /////////////////////////////////////////////////////////////////////////////
--- experiment/procedure.md
+++ experiment/procedure.md
@@ -2,6 +2,6 @@
 2.	Enter the value of angle of Analyser in the box given blow Analyser.<br>
 3.	Note the value of Ratio of output intensity to input intensity of the Laser light.<br>
 4.	Enter the value of angle of Analyser, angle of Polariser and ratio of output to input intensity.<br>
-5.	 Repeat the observation to complete the observation table.<br>
+5.	Repeat the observation to complete the observation table.<br>
 6.	Click the “Plot Graph” button.<br>
-7.	Calculate the slope of the graph.<br>
+7.	Calculate the slope of the graph.
